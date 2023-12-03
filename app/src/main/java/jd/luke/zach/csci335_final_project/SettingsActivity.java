28aef9aaaf7939a1fe7af9c02f74050a826963c9
JD Giovannetti <jonathangiovannetti2@gmail.com>
2023-12-02 22:42:04 -0800

    Fix dropdowns in settings

--- app/src/main/java/jd/luke/zach/csci335_final_project/SettingsActivity.java
+++ app/src/main/java/jd/luke/zach/csci335_final_project/SettingsActivity.java
@@ -22,6 +22,12 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        // get the user preferences and make the editor
+        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // define the spinners
+        // spinner for difficulty
         String[] difficultySpinner = new String[] {
                 "Easy", "Medium", "Hard"
         };
@@ -30,32 +36,42 @@ public class SettingsActivity extends AppCompatActivity {
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
                 difficultySpinner);
-
+        diffDropdown.setText(prefs.getString("diffPref", "Easy")); // set the default text on entering
         diffDropdown.setAdapter(adapter1);
+        diffDropdown.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedItem = adapter1.getItem(position);
+            editor.putString("diffPref", selectedItem);
+            editor.commit();
+        });
 
+
+        // spinner for color themes
         String[] themeSpinner = new String[] {
                 "Default", "Spring", "Summer", "Fall", "Winter"
         };
-        AutoCompleteTextView dropdownMenu = findViewById(R.id.themes_dropdown);
+        AutoCompleteTextView themeDropdown = findViewById(R.id.themes_dropdown);
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_dropdown_item,
                 themeSpinner);
+        themeDropdown.setText(prefs.getString("themePref", "Default")); // set the default text on entering
+        themeDropdown.setAdapter(adapter2);
+        themeDropdown.setOnItemClickListener((parent, view, position, id) -> {
+            String selectedItem = adapter2.getItem(position);
+            editor.putString("themePref", selectedItem);
+            editor.commit();
+        });
 
-        dropdownMenu.setAdapter(adapter2);
 
-
-        SharedPreferences prefs = getSharedPreferences("MyPrefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-
-
+        // define the switches
         sound = findViewById(R.id.SoundSwitch);
         vibration = findViewById(R.id.VibrationSwitch);
 
-        // set styling on app launched based on preferences
+        // set styling for switches based on preferences
         sound.setChecked(prefs.getBoolean("playSound", true));
         vibration.setChecked(prefs.getBoolean("doVibrate", true));
 
+        // switch listeners
         sound.setOnCheckedChangeListener((sw, isChecked)-> {
             if(isChecked) {
                 editor.putBoolean("playSound", true);
@@ -78,6 +94,7 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        // toolbar stuff
         Toolbar myToolbar = findViewById(R.id.toolbar3);
         setSupportActionBar(myToolbar);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Settings");
